af476a37ec05dd8effcc8e8182e11b29b9c57ade
Terry Wade <terry.wade@valpo.edu>
2018-06-18 14:24:55 -0400

    Add Route and NavLink for GitHub

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import {Route, Switch, NavLink} from 'react-router-dom';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>API Party ;)</h1>
+
+        <ul>
+          <li>
+            <NavLink to="/github">GitHub</NavLink>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path = '/github' render = {() => (<h1>GitHub!</h1>)} />
+          <Route render = {() => (<p>Click one of the links above</p>) } />
+        </Switch>
+      
       </div>
     );
   }
