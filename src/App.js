2579ea383b6ffd4c611a5230e82f255669e8c375
Terry Wade <terry.wade@valpo.edu>
2018-06-18 14:51:44 -0400

    Update State username from from

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Route, Switch, NavLink } from 'react-router-dom';
 
-import {Route, Switch, NavLink} from 'react-router-dom';
+import './App.css';
+import Github from './Github';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className = 'App-header'>
-        <h3>Aint no party like an API party</h3>
-          <h1>API Party ;)</h1>
+        <div className="App-header">
+          <h3>Ain't no party like an</h3>
+          <h1>API Party</h1>
         </div>
-
-        <ul className = 'navLinks'>
+        <ul className="navLinks">
           <li>
             <NavLink to="/github">GitHub</NavLink>
           </li>
         </ul>
-
         <Switch>
-          <Route path = '/github' render = {() => (<h1>GitHub!</h1>)} />
-          <Route render = {() => (<p>Click one of the links above</p>) } />
+          <Route path="/github" component={Github} />
+          <Route
+            render={() => (
+              <p>To get started, click one of the links above.</p>
+            )}
+          />
         </Switch>
-      
       </div>
-    );
+    )
   }
 }
-
-export default App;
+export default App
